9787ce48735f43b7bd5e51ef5c1076c15a3939c9
Ole Backhaus <ole.backhaus@gmx.de>
2024-11-26 18:29:31 +0100

    correct spelling of "applications" now get request works fine for getAll

--- backend/src/main/java/de/neuefische/backend/model/ApplicationModel.java
+++ backend/src/main/java/de/neuefische/backend/model/ApplicationModel.java
@@ -1,7 +1,11 @@
 package de.neuefische.backend.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "applications")
 public record ApplicationModel(
-        String id,
+        @Id String id,
         String company_name,
         Enums.Status status
 ) {
