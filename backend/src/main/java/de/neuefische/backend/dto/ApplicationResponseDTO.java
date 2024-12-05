2bebb7bb5d9b46424ed9461110989fcd3f4c032a
Ole Backhaus <ole.backhaus@gmx.de>
2024-12-05 11:39:11 +0100

    add @Data annotation

--- backend/src/main/java/de/neuefische/backend/dto/ApplicationResponseDTO.java
+++ backend/src/main/java/de/neuefische/backend/dto/ApplicationResponseDTO.java
@@ -1,10 +1,12 @@
 package de.neuefische.backend.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class ApplicationResponseDTO {
     private String id;
     private String companyName;
