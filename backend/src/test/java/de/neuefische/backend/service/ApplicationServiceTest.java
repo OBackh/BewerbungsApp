e7890b2e17bb69487017425281d4c4f2d15b5b37
Ole Backhaus <ole.backhaus@gmx.de>
2024-12-10 12:33:18 +0100

    add testGetAllApplications, some styling, updated pom.xml for mockito

---
+++ backend/src/test/java/de/neuefische/backend/service/ApplicationServiceTest.java
@@ -0,0 +1,36 @@
+package de.neuefische.backend.service;
+
+import de.neuefische.backend.model.ApplicationModel;
+import de.neuefische.backend.repository.ApplicationRepo;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ApplicationServiceTest {
+
+    private final ApplicationRepo mockRepo = mock(ApplicationRepo.class);
+
+    private final IdService mockIdService = mock(IdService.class);
+
+    @Test
+    void testGetAllApplications() {
+        //GIVEN
+        List<ApplicationModel> testdata = List.of(
+                new ApplicationModel("abcd123", "Firma1", "ACTIVE"),
+                new ApplicationModel("abcd246", "Firma2", "SUCCESSFUL")
+        );
+        when(mockRepo.findAll()).thenReturn(testdata);
+
+        //WHEN
+        ApplicationService underTest = new ApplicationService(mockIdService, mockRepo);
+        List<ApplicationModel> actual = underTest.getAllApplications();
+        List<ApplicationModel> expected = testdata;
+
+        //THEN
+        assertEquals(expected, actual, "Die zurückgegebene Liste der Anwendungen stimmt nicht mit den erwarteten Daten überein");
+    }
+}
