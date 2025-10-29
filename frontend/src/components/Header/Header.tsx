eabb319661bc094ac9ee5cb377cd67d36da2bde6
OBackh <ole.backhaus@gmx.de>
2025-10-30 00:35:20 +0100

    adjust spelling of bemo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import './header.css';
 export default function Header(){
     return (
             <div className="header">
-                <h1>BeMo – Der <span className="word-beginning">Be</span>werbungs<span className="word-beginning">Mo</span>nitor</h1>
+                <h1>Bemo – Der <span className="word-beginning">Be</span>werbungs<span className="word-beginning">mo</span>nitor</h1>
             </div>
     );
 
